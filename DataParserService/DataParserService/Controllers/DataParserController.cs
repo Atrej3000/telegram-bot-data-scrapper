b2b307b118aeff5a97b1db48ddf34724b201b034
Andrii Serha <andrej.serga@gmail.com>
2023-01-24 15:37:50 +0200

    Initial

---
+++ DataParserService/DataParserService/Controllers/DataParserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataParserService.Controllers
+{
+    [Route("api/[controller]")]
+    public class DataParserController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
+
